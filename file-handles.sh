cd03afed724422793e899a8517519c0ae1bdfbc6
McFrankline <mcfrankline@gmail.com>
2017-02-05 20:05:17 +0000

    Update file-handles

--- file-handles.sh
+++ file-handles.sh
@@ -1,40 +1,37 @@
 #!/usr/bin/env sh
 
 # Exit on error
-# set -e
-#
-#
-# # On MacOS, make sure shuf command is available
-# if uname | grep -q Darwin
-# then
-#   which brew &> /dev/null || echo "please install brew: https://brew.sh" &&
-    #   exit 1
-#     which gshuf &> /dev/null || brew install coreutils
-#       alias shuf=gshuf
-#       fi
-#
-#
-#       # All possible combinations of 4 letters,
-#       # each on one line for streaming and in random order
-#       combinations="$(echo {a..z}{a..z}{a..z}{a..z} | tr " " "\n" | shuf)"
-#
-#       echo "possibilities: $(echo "$combinations" | wc -l)"
-#
-#
-#       # Filter to only names that 404 on github.com,
-#       # 10 in parallel (might still take a while),
-#       # and write them to a file
-#       echo "$combinations" | xargs -P10 -I{} sh -c "curl -sfI
-#       https://github.com/{} > /dev/null || echo {}" > github.txt
-#
-#       echo "available on Github: $(wc -l github.txt)"
-#
-#
-#       # Do the same thing for twitter.com
-#       cat github.txt | xargs -P10 -I{} sh -c "curl -sfI
-#       https://twitter.com/{} > /dev/null || echo {}" > github-twitter.txt
-#
-#       echo "available on Github and Twitter: $(wc -l github-twitter.txt)"
-#
-#
-#       echo "See *.txt files for available names"
+set -e
+
+
+# On MacOS, make sure shuf command is available
+if uname | grep -q Darwin
+then
+  which brew &> /dev/null || echo "please install brew: https://brew.sh" && exit 1
+  which gshuf &> /dev/null || brew install coreutils
+  alias shuf=gshuf
+fi
+
+
+# All possible combinations of 4 letters,
+# each on one line for streaming and in random order
+combinations="$(echo {a..z}{a..z}{a..z}{a..z} | tr " " "\n" | shuf)"
+
+echo "possibilities: $(echo "$combinations" | wc -l)"
+
+
+# Filter to only names that 404 on github.com,
+# 10 in parallel (might still take a while),
+# and write them to a file
+echo "$combinations" | xargs -P10 -I{} sh -c "curl -sfI https://github.com/{} > /dev/null || echo {}" > github.txt
+
+echo "available on Github: $(wc -l github.txt)"
+
+
+# Do the same thing for twitter.com
+cat github.txt | xargs -P10 -I{} sh -c "curl -sfI https://twitter.com/{} > /dev/null || echo {}" > github-twitter.txt
+
+echo "available on Github and Twitter: $(wc -l github-twitter.txt)"
+
+
+echo "See *.txt files for available names"
